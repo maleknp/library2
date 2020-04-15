b90f4c951320f0152ba46e731ce78855010a3959
Malek AlNapouli <maleknp127@gmail.com>
2020-04-15 21:51:00 +0200

    Procfile  added

--- resources/views/contact-form.blade.php
+++ resources/views/contact-form.blade.php
@@ -1,8 +1,14 @@
 
-    #From Library#<br>
-    <strong>Name</strong>:: {{ $data['name'] }}<br>
-    <strong>E-mail</strong>:: {{ $data['email'] }}<br>
-    <strong>Message</strong>:: {{ $data['message'] }}
+    <body style="background: #380363d3;">
+    <div style="text-align: center;" class="container text-center">
+    <h1 style=" color: rgb(230, 156, 20); padding-top:10px">Library</h1>
+    <div style="color: rgb(230, 156, 20); padding-top: -20px; padding-bottom: 10px;">
+    <h4><strong>Name</strong>:: {{ $data['name'] }}</h4>
+    <h4><strong>E-mail</strong>:: {{ $data['email'] }}</h4>
+    <h4><strong>Message</strong>:: {{ $data['message'] }}</h4>
+    <div>
+    </div>
+    </body>
 
     
 
